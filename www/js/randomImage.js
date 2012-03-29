557391563b7abe29e4f455a974f5a99ee075bb5f
Yuko Honda <yuko@omase.com>
2012-03-30 07:35:21 +0900

    working on hide image when js works

--- www/js/randomImage.js
+++ www/js/randomImage.js
@@ -1,19 +1,17 @@
 var lastIndex = 0;
 
-function randomImage() {
+var randomImage = function () {
 	var mainImage = document.getElementById('mainImage');
 	var mainImageClip = document.getElementById('mainImageClip');
 	var imgDir = 'img/';
 	var imgArray = new Array('droiko.jpg','gplus.jpg','mozc.jpg','octocat.jpg','sadchrome.jpg','python.jpg','git.jpg','gopher.jpg','html5.jpg');
-	var imgIndex = 0;
-	
+	var imgIndex = 0;	
 	if(imgArray.length > 1) {
 		while(imgIndex == lastIndex) {
 			imgIndex = Math.floor(Math.random() * imgArray.length);
 		}
 		lastIndex = imgIndex;	
 		var imgPath = imgDir + imgArray[imgIndex];
-
 		mainImage.src = imgPath;
 		mainImage.alt = imgArray[imgIndex];
         mainImageClip.style.backgroundImage = "url(" + imgPath + ")";
@@ -21,7 +19,14 @@ function randomImage() {
 	else {
 		return false;
 	}
+};
+
+var hideImage = function () {
+//	document.getElementById("mainImage").style.visiblity = "hidden";
+};
+
+
+window.onload = function(){
+	randomImage();
+//	hideImage();
 }
-
-window.onload = randomImage;
-
