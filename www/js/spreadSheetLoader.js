5c31c6f82d5860a23ab6aaacf81f0347bc82ce4e
Yuko Honda <yuko@omase.com>
2012-08-07 19:22:37 +0900

    machigaetakedo commit

--- www/js/spreadSheetLoader.js
+++ www/js/spreadSheetLoader.js
@@ -10,12 +10,26 @@ function getImageDataListFromJSON(json) {
 		};
 		imageDataList.push(imageData);
 	}
+//	console.log(imageDataList);
 	return imageDataList;
 }
 
 function setImageList(anImageList) {
-	$('.entryImage').attr('src', anImageList[0].imageURL);
-	$('.entryTitle').text(anImageList[0].title);
+	var contentBody = $('#contentBody');
+	var div = $('<div class="latte"></div>');
+	var entryImageBlock = $('<div class="entryImageBlock"></div>');
+	var entryImage = $('<img class="entryImage" src="" width="612" alt="">');
+	var entryTitle = $('<p class="entryTitle"></p>');
+	for (var i = 0, l = anImageList.length; i < l; i++) {
+		$(div).append(entryImageBlock);
+		$(entryImageBlock).append(entryImage);
+		$('.entryImage').attr('src', anImageList[i].imageURL);
+		$('.entryTitle').text(anImageList[i].title);
+		// これらをまとめて var entry に代入したい
+		// cloneNode(true)
+		console.log($(div).append(entryImageBlock));
+	}
+	$(div).show();
 }
 
 // var URL = 'https://spreadsheets.google.com/feeds/list/0Ak1KvWLO65McdGtaMC05dGh5andQWHpiNHpLX1VxZnc/od6/public/basic?alt=json'
