b976b1d7bd1d00f5efebc2949fff17106671f7a5
Yuko Honda <yuko@omase.com>
2012-08-14 12:46:06 +0900

    modified entry order.

--- www/js/spreadSheetLoader.js
+++ www/js/spreadSheetLoader.js
@@ -10,23 +10,27 @@ function getImageDataListFromJSON(json) {
 		};
 		imageDataList.push(imageData);
 	}
-	console.log(imageDataList);
 	return imageDataList;
 }
 
 function setImageList(anImageList) {
 	var latte = $('.latte');
 	for (var i = 0, l = anImageList.length; i < l; i++) {
-		cloneEntry(i);
+		//cloneEntry(i);
+		latte = cloneEntry(i);
 	};
 	function cloneEntry(listOrder) {
 		var clonedEntry = $(latte).clone(true);
+		//clonedEntry.insertBefore(latte);
 		clonedEntry.insertAfter(latte);
 		clonedEntry.find('.entryImage').attr('src', anImageList[listOrder].imageURL);
-		clonedEntry.find('.entryTitle').text(anImageList[listOrder].title);	
+		clonedEntry.find('.entryTitle').text(anImageList[listOrder].title);
+		console.log(clonedEntry);
+		return clonedEntry;
 	}
 //	$('#contentBody :first').hide();
-	$('.latte').first().hide(); 
+	$('.latte').first().hide();
+	//$('.latte').last().hide(); 
 }
 
 // var URL = 'https://spreadsheets.google.com/feeds/list/0Ak1KvWLO65McdGtaMC05dGh5andQWHpiNHpLX1VxZnc/od6/public/basic?alt=json'
