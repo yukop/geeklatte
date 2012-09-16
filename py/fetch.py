693a7554f56f6aeb448649c51c25a481dd6cb19b
Yuko Honda <yuko@omase.com>
2012-09-16 17:19:30 +0900

    unified quotes to single quotes. use underscore instead of camelCase.

--- py/fetch.py
+++ py/fetch.py
@@ -2,22 +2,22 @@
 # coding: utf-8
 
 import urllib2
-import apikey
 import json
+import apikey
 
 URL = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apikey.APIKEY + '&photoset_id=72157628494205581&extras=url_o%2C+url_m&format=json&nojsoncallback=1'
 
 f = urllib2.urlopen(URL)
 data = f.read()
 dictFromJson = json.loads(data)
-photoset = dictFromJson["photoset"]
-photos = photoset["photo"]
+photoset = dictFromJson['photoset']
+photos = photoset['photo']
 photos.reverse()
 
 entry = u'''
 <div class="latte">
-<a href="http://www.flickr.com/photos/%(ownername)s/%(photo_id)s/in/set-%(photoset_id)s/" target="_blank" class="entryUrl"><img src="%(imageUrl)s" class="entryImage" alt="geeklatte"></a>
-<p class="entryTitle">%(entryTitle)s</p>
+<a href="http://www.flickr.com/photos/%(ownername)s/%(photo_id)s/in/set-%(photoset_id)s/" target="_blank" class="entryUrl"><img src="%(image_url)s" class="entryImage" alt="geeklatte"></a>
+<p class="entryTitle">%(entry_title)s</p>
 </div>
 '''
 header = u'''
@@ -35,23 +35,22 @@ header = u'''
 <body class="gradientbg">
 <div id="contentBody">
 '''
-
 footer = u'''
 <!--/#contentBody--></div>
 </body>
 </html>
 '''
 
-tosave = open('../archives.html', 'w')
-tosave.write(header.encode("utf-8"))
+tosave = open('../www/archives.html', 'w')
+tosave.write(header.encode('utf-8'))
 for photo in photos:
   photo_entry = entry % { 
-    "ownername":  photoset["ownername"],
-    "photoset_id" : photoset["id"],
-    "photo_id": photo["id"],
-    "imageUrl": photo["url_m"],
-    "entryTitle": photo["title"],
+    'ownername':  photoset['ownername'],
+    'photoset_id' : photoset['id'],
+    'photo_id': photo['id'],
+    'image_url': photo['url_m'],
+    'entry_title': photo['title'],
     }
-  tosave.write(photo_entry.encode("utf-8"))
+  tosave.write(photo_entry.encode('utf-8'))
 tosave.write(footer)
 tosave.close
