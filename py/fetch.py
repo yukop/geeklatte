5e936735a519ef329202785e7908441ecdc19d3e
Yuko Honda <yuko@omase.com>
2012-09-20 02:43:07 +0900

    fixed phase 1.5

--- py/fetch.py
+++ py/fetch.py
@@ -18,7 +18,7 @@ photos.reverse()
 entry = u'''
 <div class="latte">
 <a href="http://www.flickr.com/photos/%(ownername)s/%(photo_id)s/in/set-%(photoset_id)s/" target="_blank" class="entryUrl"><img src="%(image_url)s" class="entryImage" alt="%(entry_title)s"></a>
-<p class="entryTitle">%(entry_title)s / %(datetaken)s / %(tags)s</p>
+<p class="entryTitle">%(entry_title)s / %(datetaken)s<span class="hide"> %(tags)s</span></p>
 </div>
 '''
 header = u'''
@@ -26,24 +26,62 @@ header = u'''
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Geek Latte</title>
-<link href="css/archives.css" rel="stylesheet" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/jquery.incrementalFilter.js"></script>
 <script src="js/ga.js"></script>
-
+<link href="css/index.css" rel="stylesheet" type="text/css">
+<script>
+$(function(){
+    $('.filterBox').incrementalFilter('.latte');
+})
+</script>
 </head>
 <body class="gradientbg">
-<div id="contentBody">
+<div class="body">
+<h1 class="pageTitle">Geeklatte.com</h1>
+<form class="search"><input type="text" name="" value="" placeholder="Search" class="filterBox"></form>
+
+<div class="contentBody">
 '''
 footer = u'''
-<!--/#contentBody--></div>
+</div>
+</div>
+
+<div class="footer">
+<div class="contentUnit">
+<h2 class="title">How?</h2>
+<iframe width="480" height="270" src="http://www.youtube.com/embed/aFinw4Sb-Wk?theme=light&color=white" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<div class="contentUnit">
+<h2 class="title">Why?</h2>
+<p>
+I've been loving the internet for its openness and freedom. I don't have strong technical skill and knowledge, but I want to express my love by something I can.<br>
+Besides, I'm addicted to coffee for many years. I once had worked as a server at French Cafe at Tokyo for 2 years.<br>
+That is why I make geek latte art.
+</p>
+
+<h2 class="title">Who?</h2>
+<p>Yuko Honda is somewhere on the Internet.<br>Please feel free to contact me with next latte theme. :)</p>
+<ul class="socialLink">
+<li><a id="gp" href="https://plus.google.com/u/0/106825171914368756519/posts">Yuko Honda on Google+</a></li>
+<li><a id="tw" href="https://twitter.com/yukop">@yukop on Twitter</a></li>
+<li><a id="fr" href="http://www.flickr.com/photos/yukop/">yukop on Flickr</a></li>
+<li><a id="gh" href="https://github.com/yukop/">yukop on Github</a></li>
+</ul>
+</div>
+
+</div>
+
+</div>
 </body>
 </html>
 '''
 
-# all items for archives.html
-tosave = open('../www/archives.html', 'w')
+# all items for index.html
+tosave = open('../www/index.html', 'w')
 tosave.write(header.encode('utf-8'))
 for photo in photos:
   photo_entry = entry % { 
@@ -58,11 +96,3 @@ for photo in photos:
   tosave.write(photo_entry.encode('utf-8'))
 tosave.write(footer)
 tosave.close
-
-# latest 5 items for index.html
-del photos[10:len(photos)]
-latest10 = json.dumps(photoset)
-
-tosaveJson = open('../www/js/latestEntries.json', 'w')
-tosaveJson.write(latest10)
-tosaveJson.close
