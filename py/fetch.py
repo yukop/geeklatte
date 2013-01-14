5132dd47d3a16befdab321e257a277a1973666f5
Yuko Honda <yuko@omase.com>
2013-01-14 18:47:09 +0900

    use jinja2

--- py/fetch.py
+++ py/fetch.py
@@ -1,10 +1,11 @@
-#!/usr/bin python
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import urllib2
 import json
 import re
 import ConfigParser
+from jinja2 import Environment, FileSystemLoader
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('geeklatte.conf'))
@@ -27,113 +28,19 @@ save_jsonp.write(data2)
 save_jsonp.close
 
 
+env = Environment(loader = FileSystemLoader('../templates'))
+template = env.get_template('index.html')
+
 dictFromJson = json.loads(data)
 photoset = dictFromJson['photoset']
 photos = photoset['photo']
+for photo in photos:
+  photo['datetaken'] = re.sub('\s\d\d:\d\d:\d\d', '', photo['datetaken'],)
 photos.reverse()
 
-entry = u'''
-<div class="latte" id="%(photo_id)s">
-<a href="http://www.flickr.com/photos/%(ownername)s/%(photo_id)s/in/set-%(photoset_id)s/" target="_blank" class="entryUrl"><img src="%(thumbnail_url)s" class="entryImage" alt="%(entry_title)s"></a>
-<p class="entryTitle">%(entry_title)s / %(datetaken)s<span class="hide"> %(tags)s</span></p>
-</div>
-'''
-header = u'''
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="keywords" content="geeklatte, Geek Latte, Latte Art, ギークラテ, ラテアート">
-<meta name="description" content="I make geek latte art. ギークラテ作ってるよー！すなわち、ギークっぽい題材でラテアートしてます。">
-<title>Geeklatte</title>
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script src="js/jquery.incrementalFilter.js"></script>
-<script src="js/ga.js"></script>
-<link href="css/index.css" rel="stylesheet" type="text/css">
-<script>
-$(function(){
-    $('.filterBox').incrementalFilter('.latte');
-})
-</script>
-</head>
-<body class="gradientbg">
-<div class="body">
-<h1 class="pageTitle">Geeklatte</h1>
-<form class="search"><input type="text" name="" value="" placeholder="Search" class="filterBox"></form>
-
-<div class="contentBody">
-'''
-footer = u'''
-</div>
-</div>
-
-<div class="footer">
-<div class="contentUnit">
-<h2 class="title">How?</h2>
-<iframe width="480" height="270" src="http://www.youtube.com/embed/aFinw4Sb-Wk?theme=light&color=white" frameborder="0" allowfullscreen></iframe>
-</div>
-
-<div class="contentUnit">
-<h2 class="title">Why?</h2>
-<p>
-I love the Internet. I love its openness and its favor of information freedom. I thought I could somehow express my gratitude and respects for geeks who are involved in the Internet technology.<br>
-Besides that, I'm a long time coffee addict. And now, I'm fortunate enough to have a good Espresso machine in my office. The time has come!<br>
-This is why I make this series of "geek" latte art.
-</p>
-
-<h2 class="title">Who?</h2>
-<p>Yuko Honda lives in somewhere on the Internet.<br>If you come up with any latte art ideas, please let me know. :)</p>
-<ul class="socialLink">
-<li><a id="gp" href="https://plus.google.com/u/0/106825171914368756519/posts">Yuko Honda on Google+</a></li>
-<li><a id="tw" href="https://twitter.com/yukop">@yukop on Twitter</a></li>
-<li><a id="fr" href="http://www.flickr.com/photos/yukop/">yukop on Flickr</a></li>
-<li><a id="gh" href="https://github.com/yukop/">yukop on Github</a></li>
-<li><a id="in">Share Link</a></li>
-</ul>
-
-</div>
-</div>
-</div>
-<script>
-$(function(){
-  $('.filterBox').incrementalFilter('.latte');
-
-    window.Intent = window.Intent || window.WebKitIntent;
-    window.navigator.startActivity = window.navigator.startActivity || window.navigator.webkitStartActivity;
-    window.intent = window.intent || window.webkitIntent;
-
-    $("#in").click(setIntent)
-    function setIntent(){
-      var intent = new Intent(
-          "http://webintents.org/share",
-          "text/uri-list",
-          "http://geeklatte.com");
-      window.navigator.startActivity(intent);
-      return false;
-    };
-});
-</script>
-</body>
-</html>
-'''
-
-# all items for index.html
+output = template.render(photos=photos, photoset=photoset)
 save_html = open('../public_html/index.html', 'w')
-save_html.write(header.encode('utf-8'))
-for photo in photos:
-  photo_entry = entry % { 
-    'ownername':  photoset['ownername'],
-    'photoset_id' : photoset['id'],
-    'photo_id': photo['id'],
-    'thumbnail_url': photo['url_s'],
-    'image_url': photo['url_o'],
-    'entry_title': photo['title'],
-    'tags': photo['tags'],
-    'datetaken': re.sub('\s\d\d:\d\d:\d\d', '', photo['datetaken'],)
-    }
-  save_html.write(photo_entry.encode('utf-8'))
-save_html.write(footer)
-save_html.close
+save_html.write(output.encode('utf-8'))
+
 
 
